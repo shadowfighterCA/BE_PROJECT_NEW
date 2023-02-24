5307132287c3e795d135e1d114a804063775df31
advait-1820 <117989317+shadowfighterCA@users.noreply.github.com>
2023-02-24 23:00:53 +0530

    BE project

---
+++ nvs.h
@@ -0,0 +1,18 @@
+#ifndef NVS_H
+#define NVS_H
+
+#include <nvs_flash.h>
+#include <Preferences.h>
+
+#define EEPROM_SIZE 50
+#define ALCOHOL_KEY "jdnsj"
+#define CABIN_TEMP_KEY "jikld"
+#define ENGINE_TEMP_KEY "buhdk"
+#define CABIN_HUMIDITY_KEY "debhr"
+#define GPS_LONG_KEY "fjdfj"
+#define GPS_LAT_KEY  "erujh"
+
+Preferences preferences;
+
+
+#endif
